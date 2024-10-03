43f23d75d4c0f439a2274845d1c7040abab7ad58
Chad Seng <chanrung1234@gmail.com>
2024-10-03 14:23:22 -0500

    refactor packagejson and refactor and starting fetch g3d

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,5 @@ import ReactDOM from "react-dom/client";
 import App from "./components/App.tsx";
 import "./index.css";
 import { StrictMode } from "react";
-import { Buffer } from "buffer";
-globalThis.Buffer = Buffer;
+
 ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
