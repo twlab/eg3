5511cd5abd809a042b80bc5fa7394873a825eb12
Chad S <chanrung1234@gmail.com>
2025-05-24 23:12:03 -0500

    fix package and config

--- eg-browser/vite.config.ts
+++ eg-browser/vite.config.ts
@@ -1,44 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
-import dts from "vite-plugin-dts";
 
 import tailwindcss from "@tailwindcss/vite";
-import url from "@rollup/plugin-url";
+
 import { nodePolyfills } from "vite-plugin-node-polyfills";
+
 export default defineConfig({
+  plugins: [react(), nodePolyfills(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
-    extensions: [".js", ".json", ".jsx", ".mjs", ".ts", ".tsx", ".vue"],
   },
-
-  build: {
-    lib: {
-      entry: path.resolve(__dirname, "src/index.ts"),
-      name: "browser",
-      fileName: (format) => `index.${format}.js`,
-    },
-    rollupOptions: {
-      external: ["react", "react-dom"],
-      output: {
-        globals: {
-          react: "React",
-          "react-dom": "ReactDOM",
-        },
-      },
-    },
-  },
-  plugins: [
-    tailwindcss(),
-    url(),
-    react(),
-    nodePolyfills(),
-    dts({
-      outputDir: "dist",
-      entryRoot: "src",
-    }),
-  ],
   base: "/browser/",
 });
