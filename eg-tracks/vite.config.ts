ae0a72b0990502a45eedd7ac18666f5699138b75
ChadS <chanrung1234@gmail.com>
2025-09-05 15:18:26 -0500

    increase initial load

--- eg-tracks/vite.config.ts
+++ eg-tracks/vite.config.ts
@@ -15,4 +15,27 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+          d3: ['d3'],
+          gmod: ['@gmod/bam', '@gmod/bbi', '@gmod/tabix', '@gmod/twobit', '@gmod/vcf'],
+          pixi: ['pixi.js'],
+          utils: ['lodash', 'axios', 'jquery'],
+        },
+      },
+    },
+    chunkSizeWarningLimit: 1000,
+  },
+  optimizeDeps: {
+    include: ['react', 'react-dom', 'd3', 'pixi.js', 'lodash'],
+    exclude: ['@gmod/bam', '@gmod/bbi'],
+  },
+  server: {
+    hmr: {
+      overlay: false,
+    },
+  },
 });
