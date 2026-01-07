61f634d0cecb1d1930b2f2b4250948405587b5ca
Weezsle <chanrung1234@gmail.com>
2026-01-07 15:21:08 -0800

    working annotation

--- eg-browser/src/components/root-layout/tabs/tracks/destinations/AnnotationTracks.tsx
+++ eg-browser/src/components/root-layout/tabs/tracks/destinations/AnnotationTracks.tsx
@@ -1,5 +1,5 @@
 import { useMemo } from "react";
-
+import { getGenomeConfig } from "wuepgg3-track";
 import CollectionView, {
   ICollectionViewDataSource,
 } from "@/components/ui/collection/CollectionView";
@@ -19,40 +19,110 @@ export default function AnnotationTracks() {
     return new Set(session?.tracks.map((track) => track.name));
   }, [session?.tracks]);
 
-  const data = useMemo(() => {
-    const tracks = genomeConfig?.annotationTracks || {};
+  const combinedGenomeData = useMemo(() => {
+    const querySet = new Set<string>();
+    const resultArr: Array<any> = [];
 
-    return Object.entries(tracks).flatMap(([section, value]) => {
-      let trackList: any[] = [];
+    // find annotation from primary genome
+    if (genomeConfig) {
+      const tracks = genomeConfig.annotationTracks || {};
+      resultArr.push({
+        genomeName: genomeConfig.name ?? "none",
+        tracks: Object.entries(tracks).flatMap(([section, value]) => {
+          let trackList: any[] = [];
 
-      if (Array.isArray(value) && value.length > 0 && Array.isArray(value[0])) {
-        trackList = value.flat();
-      } else if (
-        !Array.isArray(value) &&
-        typeof value === "object" &&
-        value !== null
-      ) {
-        trackList = Object.values(value as Record<string, unknown>).flat();
-      } else {
-        trackList = Array.isArray(value) ? value : [value];
+          if (
+            Array.isArray(value) &&
+            value.length > 0 &&
+            Array.isArray(value[0])
+          ) {
+            trackList = value.flat();
+          } else if (
+            !Array.isArray(value) &&
+            typeof value === "object" &&
+            value !== null
+          ) {
+            trackList = Object.values(value as Record<string, unknown>).flat();
+          } else {
+            trackList = Array.isArray(value) ? value : [value];
+          }
+
+          return trackList.map((track) => ({
+            section,
+            genomeName: genomeConfig.name ?? "none",
+            id: track.name || track.url,
+            title: track.label ?? track.name,
+            data: track,
+          }));
+        }),
+      });
+    }
+
+    // find queryGenome genome geomealign
+    if (session?.tracks && genomeConfig) {
+      for (let i = 0; i < session.tracks.length; i++) {
+        const track = session.tracks[i];
+        const queryGenome = track["querygenome"]
+          ? track.querygenome
+          : track.metadata?.genome
+          ? track.metadata.genome
+          : null;
+        if (
+          queryGenome &&
+          queryGenome !== genomeConfig.name &&
+          !querySet.has(queryGenome)
+        ) {
+          const queryGenomeConfig = getGenomeConfig(queryGenome);
+          const queryAnnotationTracks =
+            queryGenomeConfig?.annotationTracks || {};
+          querySet.add(queryGenome);
+          resultArr.push({
+            genomeName: queryGenome,
+            tracks: Object.entries(queryAnnotationTracks).flatMap(
+              ([section, value]) => {
+                let trackList: any[] = [];
+
+                if (
+                  Array.isArray(value) &&
+                  value.length > 0 &&
+                  Array.isArray(value[0])
+                ) {
+                  trackList = value.flat();
+                } else if (
+                  !Array.isArray(value) &&
+                  typeof value === "object" &&
+                  value !== null
+                ) {
+                  trackList = Object.values(
+                    value as Record<string, unknown>
+                  ).flat();
+                } else {
+                  trackList = Array.isArray(value) ? value : [value];
+                }
+
+                return trackList.map((track) => ({
+                  section,
+                  genomeName: queryGenomeConfig?.genome.getName() ?? "none",
+                  id: track.name || track.url,
+                  title: track.label ?? track.name,
+                  data: track,
+                }));
+              }
+            ),
+          });
+        }
       }
+    }
 
-      return trackList.map((track) => ({
-        section,
-        genomeName: genomeConfig && genomeConfig.name ? genomeConfig.name : "none",
-        id: track.name || track.url,
-        title: track.label ?? track.name,
-        data: track,
-      }));
-    });
-  }, [genomeConfig]);
+    return resultArr;
+  }, [genomeConfig, session?.tracks]);
 
   const handleSelect = (item: ICollectionViewDataSource<any>) => {
     const track = { ...item.data };
 
     track.metadata = {
       ...track.metadata,
-      genome: genomeConfig && genomeConfig.name ? genomeConfig.name : "none",
+      genome: item && item.genomeName ? item.genomeName : "none",
     };
 
     dispatch(
@@ -63,10 +133,15 @@ export default function AnnotationTracks() {
   };
 
   return (
-    <CollectionView
-      data={data}
-      selectedIds={selectedIds}
-      onSelect={handleSelect}
-    />
+    <>
+      {combinedGenomeData.map((genomeData) => (
+        <CollectionView
+          key={genomeData.genomeName}
+          data={genomeData.tracks}
+          selectedIds={selectedIds}
+          onSelect={handleSelect}
+        />
+      ))}
+    </>
   );
 }
--- eg-tracks/src/models/Feature.ts
+++ eg-tracks/src/models/Feature.ts
@@ -123,11 +123,6 @@ export class Feature {
    * @return {OpenInterval[]} coordinates in the navigation context
    */
   computeNavContextCoordinates(navContext: NavigationContext) {
-    console.log(
-      this.getLocus(),
-      navContext,
-      navContext.convertGenomeIntervalToBases(this.getLocus())
-    );
     return navContext.convertGenomeIntervalToBases(this.getLocus());
   }
 }
