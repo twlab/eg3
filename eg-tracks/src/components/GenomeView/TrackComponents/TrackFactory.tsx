9b3b845d7dcce80494199e3b3f14be0cbde3d79a
Chad <chanrung1234@gmail.com>
2026-04-23 11:38:08 -0500

    fix screenshot

--- eg-tracks/src/components/GenomeView/TrackComponents/TrackFactory.tsx
+++ eg-tracks/src/components/GenomeView/TrackComponents/TrackFactory.tsx
@@ -589,7 +589,7 @@ const TrackFactory: React.FC<TrackProps> = memo(function TrackFactory({
                   : getConfigOptions().height,
               trackModel,
               basesByPixel: basePerPixel,
-              genomeConfig,
+              genomeConfig : _.cloneDeep(genomeConfig),
               xvaluesData: cacheTrackData[dataIdx].xvalues
                 ? cacheTrackData[dataIdx].xvalues
                 : null,
